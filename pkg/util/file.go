dde2e848f918c5c2d8840e24820f00280aca144b
Siphalor <info@siphalor.de>
2021-10-13 13:29:59 +0200

    Improve FileExists error handling

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
@@ -8,11 +9,8 @@ import (
 func FileExists(path string) (bool, error) {
 	stat, err := os.Stat(path)
 	if err != nil {
-		if os.IsNotExist(err) {
+		if errors.Is(err, os.ErrNotExist) {
 			return false, nil
-		}
-		if os.IsExist(err) {
-			return true, nil
 		}
 		return false, fmt.Errorf("failed to check if file exists %w", err)
 	}
