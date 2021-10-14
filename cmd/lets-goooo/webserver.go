b05b88384771404c4592486447a7e7471d3c3441
DJJMMusik <72066315+DJJMMusik@users.noreply.github.com>
2021-10-14 18:33:44 +0200

    default Site generated

--- cmd/lets-goooo/webserver.go
+++ cmd/lets-goooo/webserver.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"sync"
 	"time"
 )
+
+const tempDefaultString = `<html>
+<head>
+<title>Lets Goooo</title>
+</head>
+<body style="text-align:center">
+<img src="assets/logoooo.svg" style="max-width:500px;" alt="Logo" />
+</body>
+</html>`
+
+var tempDefault = template.Must(template.New("lets goooo").Parse(tempDefaultString)) //TODO get from other file
 
 func RunWebservers() {
 	wait := new(sync.WaitGroup)
@@ -22,17 +35,20 @@ func RunWebservers() {
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	name := q.Get("name")
-	if name == "" {
-		name = "World"
-	}
-	responseString := "<html><body>Hello " + name + "</body></html>"
-	w.Write([]byte(responseString)) // unbedingt Templates verwenden!
+	tempDefault.ExecuteTemplate(w, "lets goooo", nil)
+
+	//q := r.URL.Query()
+	//name := q.Get("name")
+	//if name == "" {
+	//	name = "World"
+	//}
+	//responseString := "<html><body>Hello " + name + "</body></html>"
+	//w.Write([]byte(responseString)) // unbedingt Templates verwenden!
 }
 
 func CreateWebserver(port int) {
 	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
 	mux.HandleFunc("/", mainHandler) //TODO mainHandler für LogoSeite
 	//http.HandleFunc("/login/", loginHandler)
@@ -43,6 +59,6 @@ func CreateWebserver(port int) {
 		Handler: mux,
 	}
 
-	server.ListenAndServeTLS("certification/cert.pem", "certification/key.pem")
+	log.Fatalln(server.ListenAndServeTLS("certification/cert.pem", "certification/key.pem"))
 
 }
