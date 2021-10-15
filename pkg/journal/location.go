8507b4edda2f981a2602c05ee631d691a89d772c
LoehJ <johannes.loehj@gmail.com>
2021-10-15 14:51:38 +0200

    Read location file

--- pkg/journal/location.go
+++ pkg/journal/location.go
@@ -1,11 +1,56 @@
 package journal
 
-var Locations = map[string]*Location{ // TODO: load dynamically
-	"MOS": {Name: "Mosbach", Code: "MOS"},
-	"MGH": {Name: "Bad Mergentheim", Code: "MGH"},
-}
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var Locations map[string]*Location
 
 type Location struct {
-	Name string
-	Code string
+	XMLName xml.Name `xml:"location"`
+	Name    string   `xml:"name,attr"`
+	Code    string   `xml:"code,attr"`
 }
+
+type locationsXML struct {
+	XMLName   xml.Name   `xml:"locations"`
+	Locations []Location `xml:"location"`
+}
+
+func ReadLocations(path string) error {
+	xmlFile, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer func() {
+		err = xmlFile.Close()
+		if err != nil {
+			fmt.Printf("error occured during closing of Location XML file: %v", err)
+		}
+	}()
+
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	l := locationsXML{}
+	err = xml.Unmarshal(byteValue, &l)
+	if err != nil {
+		return fmt.Errorf("error occured during loading of Location XML file: %v", err)
+	}
+	Locations = map[string]*Location{}
+
+	for _, location := range l.Locations {
+		Locations[location.Code] = &location
+	}
+	return nil
+}
+
+/*
+<locations>
+	<location name="Mosbach" code="MOS"></location>
+	<location name="Bad Mergentheim" code="MGH"></location>
+</locations>
+*/
