0b9ce1980401a5f605883721d9f7a01294d2f837
LoehJ <johannes.loehj@gmail.com>
2021-10-18 12:03:03 +0200

    Decrypt implemented - Problems with :Location messing up

--- pkg/token/tokenGen.go
+++ pkg/token/tokenGen.go
@@ -29,3 +29,18 @@ func EncryptAES(key []byte, plaintext string) string {
 
 	return hex.EncodeToString(out)
 }
+
+func DecryptAES(key []byte, ciphertext string) string {
+
+	cipher, _ := hex.DecodeString(ciphertext)
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	plain := make([]byte, len(cipher))
+	c.Decrypt(plain, cipher)
+
+	return string(plain)
+}
