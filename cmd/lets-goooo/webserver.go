2806e3551399d7a0efacb4db49bd9efcd59dd522
DJJMMusik <72066315+DJJMMusik@users.noreply.github.com>
2021-10-19 15:19:13 +0200

    fixed qr

--- cmd/lets-goooo/webserver.go
+++ cmd/lets-goooo/webserver.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"html/template"
 	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/token"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
 
 var logIOUrl = "https://localhost"
 
-type QrCodeData struct {
-	Qrcode string
+type QrCodeUrl struct {
+	PngUrl   string
+	Location string
 }
 
 func RunWebservers() {
 	wait := new(sync.WaitGroup)
 	wait.Add(2)
 	go func() {
-		handlers := map[string]http.HandlerFunc{"/": defaultHandler}
+		handlers := map[string]http.HandlerFunc{
+			"/":       defaultHandler,
+			"/login":  loginHandler,
+			"/logout": logoutHandler,
+		}
 		CreateWebserver(443, handlers)
 		wait.Done()
 	}()
 	time.AfterFunc(2*time.Second, func() {
 		handler := map[string]http.HandlerFunc{
-			"/":   defaultHandler,
-			"/qr": qrHandler,
+			"/":       defaultHandler,
+			"/qr.png": qrPngHandler,
+			"/qr":     qrHandler,
 		}
 		CreateWebserver(4443, handler)
 		wait.Done()
@@ -40,23 +46,52 @@ func defaultHandler(w http.ResponseWriter, _ *http.Request) {
 	executeTemplate(w, "default.html", nil)
 }
 
-func qrHandler(w http.ResponseWriter, r *http.Request) {
+func loginHandler(w http.ResponseWriter, _ *http.Request) {
+	executeTemplate(w, "default.html", nil)
+}
+
+func logoutHandler(w http.ResponseWriter, _ *http.Request) {
+	executeTemplate(w, "default.html", nil)
+}
+
+func qrPngHandler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
-	location := q.Get("location")
-	qrcodeString := base64.StdEncoding.EncodeToString(token.GetQrCode(logIOUrl, location))
-	qrcode := QrCodeData{Qrcode: qrcodeString}
-	executeTemplate(w, "qr.html", qrcode)
+	location := strings.ToUpper(q.Get("location"))
+	if len(location) != 3 {
+		fmt.Printf("abbreviation has to be 3 characters, not %v", len(location))
+		if _, err := w.Write([]byte("couldn't resolve location-abbreviation. Need 3 characters")); err != nil {
+			fmt.Printf("failed to write qrcode to Response: %v", err)
+			return
+		}
+		return
+	}
+	qrcode := token.GetQrCode(logIOUrl, location)
+	/*
+		if qrcodeString, err := token.GetQrCode(logIOUrl, location); err != nil {
+			fmt.Printf("failed to get qrcode: %v", err)
+			return
+		}
+	*/
+	if _, err := w.Write(qrcode); err != nil {
+		fmt.Printf("failed to write qrcode to Response: %v", err)
+		return
+	}
+}
+
+func qrHandler(w http.ResponseWriter, r *http.Request) {
+	data := QrCodeUrl{fmt.Sprintf("%v.png", r.URL.Path), r.URL.Query().Get("location")}
+	executeTemplate(w, "qr.html", data)
 }
 
 func executeTemplate(w http.ResponseWriter, file string, data interface{}) {
-	tempDefault, parseErr := template.ParseFiles(file)
-	if parseErr != nil {
-		fmt.Printf("failed to parse template: %#v", parseErr)
+	tempDefault, err := template.ParseFiles(file)
+	if err != nil {
+		fmt.Printf("failed to parse template: %#v", err)
 		return
 	}
-	exErr := tempDefault.Execute(w, data)
-	if exErr != nil {
-		fmt.Printf("failed to execute template: %#v", exErr)
+	err = tempDefault.Execute(w, data)
+	if err != nil {
+		fmt.Printf("failed to execute template: %#v", err)
 		return
 	}
 }
