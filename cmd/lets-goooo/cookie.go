a98ec2cb0625d599e672cd8b07114b60e8903995
LoehJ <johannes.loehj@gmail.com>
2021-11-27 17:51:15 +0100

    Implemented cookie.Validate

--- cmd/lets-goooo/cookie.go
+++ cmd/lets-goooo/cookie.go
@@ -1,10 +1,29 @@
 package main
 
-import "lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/journal"
+import (
+	"fmt"
+	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/journal"
+	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/util"
+	"strings"
+)
 
-func Validate(string2 string) (journal.User , error){
+func Validate(string2 string) (journal.User, error) {
 
+	userData := strings.Split(string2, ";")
 
+	if util.Base64Encode(util.HashString(userData[0]+"\t"+secret)) == userData[1] {
+		userData0, _ := util.Base64Decode(userData[0])
+		userData = strings.Split(string(userData0), "\t")
+		user := journal.User{
+			Name:    userData[0],
+			Address: userData[1],
+		}
 
-	return nil , new error(rip)
+		return user, nil
+	}
+
+	return journal.User{
+		Name:    "",
+		Address: "",
+	}, fmt.Errorf("user Data in Cookie did not match the hash")
 }
