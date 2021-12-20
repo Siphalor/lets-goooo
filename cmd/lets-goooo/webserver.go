edfad0c90a89feb6c6ad7e5c953159993e6fc9cc
DJJMMusik <72066315+DJJMMusik@users.noreply.github.com>
2021-12-20 17:55:44 +0100

    commends and structuring

--- cmd/lets-goooo/webserver.go
+++ cmd/lets-goooo/webserver.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"html/template"
 	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/journal"
-	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/token"
-	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/util"
 	"log"
 	"net/http"
 	"runtime"
@@ -16,7 +14,7 @@ import (
 )
 
 //setting default values for global variables
-//will be overwritten in main class by flags
+//values get overwritten in main class by flags
 var logIOUrl = "https://localhost:4443/"
 var dataJournal = (*journal.Writer)(nil)
 var cookieSecret = ""
@@ -33,7 +31,7 @@ func RunWebservers(portLogin uint, portQr uint) error {
 
 	//creating webserver for QrCode
 	handlerQR := map[string]http.HandlerFunc{
-		"/":       defaultHandler,
+		"/":       homeHandler,
 		"/qr":     qrHandler,
 		"/qr.png": qrPngHandler,
 	}
@@ -73,247 +71,27 @@ func RunWebservers(portLogin uint, portQr uint) error {
 	return nil
 }
 
-func defaultHandler(w http.ResponseWriter, _ *http.Request) {
+// homeHandler creates a default response
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
 	executeTemplate(w, "default.html", nil, false)
-}
-
-func cookieHandler(w http.ResponseWriter, r *http.Request) {
-	if !r.URL.Query().Has("token") {
-		defaultHandler(w, r)
-		return
-	}
-
-	userdataCookie := (*http.Cookie)(nil)
-	userdataCookie, err := r.Cookie("Userdata")
-	if err != nil {
-		executeLogin(w, (*journal.User)(nil), r.URL.Query().Get("token"))
-		return
-	}
-	userdata, err := Validate(userdataCookie.Value)
-	if err != nil {
-		executeLogin(w, (*journal.User)(nil), r.URL.Query().Get("token"))
-		return
-	}
-	location, err := dataJournal.GetCurrentUserLocation(util.Base64Encode(userdata.Hash()))
-	if err != nil || location == nil {
-		executeLogin(w, &userdata, r.URL.Query().Get("token"))
-	} else {
-		executeLogout(w, &userdata, r.URL.Query().Get("token"))
-	}
-}
-
-func executeLogin(w http.ResponseWriter, user *journal.User, toke string) {
-	location, err := token.Validate(toke)
-	if err != nil {
-		log.Printf("invalid token: %v\n", err)
-		writeError(w, 400, "invalid token")
-		return
-	}
-
-	executeTemplate(w, "login.html", struct {
-		User     *journal.User
-		Location *journal.Location
-		Token    string
-	}{
-		User:     user,
-		Location: location,
-		Token:    toke,
-	}, false)
-}
-
-func executeLogout(w http.ResponseWriter, user *journal.User, toke string) {
-	location, err := token.Validate(toke)
-	if err != nil {
-		log.Printf("invalid token: %v\n", err)
-		writeError(w, 400, "invalid token")
-		return
-	}
-
-	executeTemplate(w, "logout.html", struct {
-		User     *journal.User
-		Location *journal.Location
-		Token    string
-	}{
-		User:     user,
-		Location: location,
-		Token:    toke,
-	}, false)
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	//check if token is valid
-	tokenString := r.URL.Query().Get("token")
-	tokenLocation, err := token.Validate(tokenString)
-	if err != nil {
-		log.Printf("invalid token: %v\n", err)
-		writeError(w, 400, "invalid token")
-		return
-	}
-
-	//create userdata
-	err = r.ParseForm()
-	if err != nil {
-		log.Printf("couldn't parse form: %v\n", err)
-		writeError(w, 400, "invalid form")
-		return
-	}
-	user := r.Form.Get("name")
-	address := r.Form.Get("address")
-
-	userdata := journal.User{
-		Name:    user,
-		Address: address,
-	}
-
-	//search for Cookie holding Userdata
-	userdataCookie, err := r.Cookie("Userdata")
-	if userdataCookie != nil {
-		userdata, err = Validate(userdataCookie.Value)
-	}
-
-	if err != nil {
-
-		data := util.Base64Encode(([]byte)(userdata.ToJournalLine()))
-		hash := util.Base64Encode(util.HashString(data + "\t" + cookieSecret))
-
-		//create cookie
-		userdataCookie := &http.Cookie{
-			Name:  "Userdata",
-			Value: data + ":" + hash,
-		}
-		http.SetCookie(w, userdataCookie)
-	}
-
-	location, _ := dataJournal.GetCurrentUserLocation(util.Base64Encode(userdata.Hash()))
-	if location != (*journal.Location)(nil) {
-		//no Location to be logged in
-		log.Printf("user is already elsewhere: %v\n", err)
-		writeError(w, 400, "already logged in")
-		return
-	}
-
-	//create entry in journal
-	err = dataJournal.WriteEventUser(&userdata, tokenLocation, journal.LOGIN)
-	if err != nil {
-		log.Printf("couldn't write into journal: %v\n", err)
-		writeError(w, 500, "failed to log in")
-		return
-	}
-
-	//return to Home
-	redirectToHome(w, 302)
-}
-
-func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	//check if token is valid
-	tokenString := r.URL.Query().Get("token")
-	tokenLocation, err := token.Validate(tokenString)
-	if err != nil {
-		log.Printf("invalid token: %v\n", err)
-		writeError(w, 400, "invalid token")
-		return
-	}
-
-	//search for Cookie holding Userdata
-	userdataCookie, err := r.Cookie("Userdata")
-	if err != nil {
-		//no User to be logged out
-		log.Printf("failed to read user cookie: %v\n", err)
-		writeError(w, 400, "invalid session")
-		return
-	}
-
-	userdata, err := Validate(userdataCookie.Value)
-
-	if err != nil {
-		//no User to be logged out
-		log.Printf("failed to read user cookie: %v\n", err)
-		writeError(w, 400, "invalid session")
-		return
-	}
-
-	//check if user is at a location
-	location, err := dataJournal.GetCurrentUserLocation(util.Base64Encode(userdata.Hash()))
-	if err != nil {
-		log.Printf("user is at no location: %v\n", err)
-		writeError(w, 400, "you're not logged in anywhere")
-		return
-	}
-
-	//check if token is valid for user location
-	if tokenLocation != location {
-		log.Printf("user is not at the token's location: %v\n", err)
-		writeError(w, 400, "trying to log out from wrong location")
-		return
-	}
-
-	//log out user
-	err = dataJournal.WriteEventUser(&userdata, location, journal.LOGOUT)
-	if err != nil {
-		log.Printf("couldn't write into journal: %v\n", err)
-		writeError(w, 500, "failed to log out")
-		return
-	}
-
-	//return to Home
-	redirectToHome(w, 302)
-}
-
-func qrPngHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	location := strings.ToUpper(q.Get("location"))
-
-	if location == "" {
-		log.Printf("there was no given location: %s\n", location)
-		writeError(w, 400, "no given location")
-		return
-	}
-
-	_, exists := journal.Locations[location]
-	if !exists {
-		fmt.Printf("failed to resolve location: %v\n", location)
-		writeError(w, 400, "unknown location")
-		return
-	}
-
-	qrcode, err := token.GetQrCode(logIOUrl, location)
-	if err != nil {
-		log.Printf("failed to get qrcode: %v\n", err)
-		writeError(w, 400, "failed to generate qr code")
-		return
-	}
-
-	if _, err := w.Write(qrcode); err != nil {
-		log.Printf("failed to write qrcode to Response: %v\n", err)
-		writeError(w, 400, "failed to send qr code")
-		return
-	}
-}
-
-func qrHandler(w http.ResponseWriter, r *http.Request) {
-	data := struct {
-		PngUrl   string
-		Location string
-	}{
-		PngUrl:   fmt.Sprintf("%s.png", r.URL.Path),
-		Location: r.URL.Query().Get("location"),
-	}
-	executeTemplate(w, "qr.html", data, false)
 }
 
 // executeTemplate creates a Template from a given file and writes the text filled with data into the http.Response
 func executeTemplate(w http.ResponseWriter, file string, data interface{}, directFilePath bool) {
 	files := []string{file}
 	if !directFilePath {
-		base := GetFilePath() + "/"
+		base := GetPathToWd() + "/"
 		files = []string{base + "template/" + file, base + "template/head.html", base + "template/footer.html"}
 	}
+
+	//Create template from file
 	temp, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Printf("failed to parse template: %v \n", err)
 		return
 	}
 
+	//Execute template with given data
 	if err := temp.Execute(w, data); err != nil {
 		log.Printf("failed to execute template: %v \n", err)
 		return
@@ -351,8 +129,9 @@ func CreateWebserver(port uint, handlers map[string]http.HandlerFunc) (*http.Ser
 	return &server, destroy
 }
 
+// RunWebserver starts the given server
 func RunWebserver(server *http.Server) error {
-	_ = GetFilePath()
+	_ = GetPathToWd()
 	err := server.ListenAndServeTLS("certification/cert.pem", "certification/key.pem")
 	if err != http.ErrServerClosed {
 		return err
@@ -360,7 +139,8 @@ func RunWebserver(server *http.Server) error {
 	return nil
 }
 
-func GetFilePath() string {
+// GetPathToWd returns the path to the Working directory
+func GetPathToWd() string {
 	_, filename, _, _ := runtime.Caller(0)
 	index := strings.LastIndex(filename, "cmd")
 	if index == -1 {
---
+++ cmd/lets-goooo/webserver_ qrCodeHandler.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/journal"
+	"lehre.mosbach.dhbw.de/lets-goooo/v2/pkg/token"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// qrPngHandler returns a picture of the qrCode
+func qrPngHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	location := strings.ToUpper(q.Get("location"))
+
+	if location == "" {
+		log.Printf("there was no given location: %s\n", location)
+		writeError(w, 400, "no given location")
+		return
+	}
+
+	_, exists := journal.Locations[location]
+	if !exists {
+		fmt.Printf("failed to resolve location: %v\n", location)
+		writeError(w, 400, "unknown location")
+		return
+	}
+
+	qrcode, err := token.GetQrCode(logIOUrl, location)
+	if err != nil {
+		log.Printf("failed to get qrcode: %v\n", err)
+		writeError(w, 400, "failed to generate qr code")
+		return
+	}
+
+	if _, err := w.Write(qrcode); err != nil {
+		log.Printf("failed to write qrcode to Response: %v\n", err)
+		writeError(w, 400, "failed to send qr code")
+		return
+	}
+}
+
+// qrHandler creates thw qrCode response with data (qrCode is generated with location in the template)
+func qrHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		PngUrl   string
+		Location string
+	}{
+		PngUrl:   fmt.Sprintf("%s.png", r.URL.Path),
+		Location: r.URL.Query().Get("location"),
+	}
+	executeTemplate(w, "qr.html", data, false)
+}
